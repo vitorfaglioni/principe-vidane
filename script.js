1e25273d0c0e63259b6e8ee3716b65343f5ae9aa
vitorfaglioni@gmail.com <vitorfaglioni@gmail.com>
2024-09-04 00:54:10 -0300

    fixed script

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Defina a URL de redirecionamento aqui
 const redirectUrl = "https://vidane.com.br";
 
-// Redireciona após 3 segundos (3000 milissegundos)
 setTimeout(function() {
     window.location.href = redirectUrl;
 }, 3000);
